1f78f92dfda9d23cd931747f60b831ce04958cef
darkydtm <darkydtm@users.noreply.github.com>
2026-06-30 02:25:44 +0500

    add miuix theme with Material You dynamic colors

--- app/src/main/java/com/anverter/app/MainActivity.kt
+++ app/src/main/java/com/anverter/app/MainActivity.kt
@@ -6,16 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.anverter.app.ui.theme.AnverterTheme
+import top.yukonga.miuix.kmp.basic.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            Placeholder()
+            AnverterTheme {
+                Placeholder()
+            }
         }
     }
 }
